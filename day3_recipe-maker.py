c5cce988f2758c3b695d51ede9eb46f78fa328e4
cliqueengagements <e037403.olawunmi@dlc.ui.edu.ng>
2022-09-30 17:34:51 +0100

    recipe maker

---
+++ day3_recipe-maker.py
@@ -0,0 +1,9 @@
+#Creating The Ultimate Wacky Recipe Maker
+
+food = input("Enter your favourite food: ")
+plant = input("Pick a plant: ")
+cooking = input("Name a cooking method: ")
+burned_food = input("How would you describe a burned food?: ")
+diy = input("Name a DIY item: ")
+print()
+print(cooking, food, "with", burned_food, plant, "on a bed of", diy)
