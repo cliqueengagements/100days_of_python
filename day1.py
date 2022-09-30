a1232302bd1bee09425909700efb3252f0fea538
cliqueengagements <e037403.olawunmi@dlc.ui.edu.ng>
2022-09-30 17:44:35 +0100

    first commit

---
+++ day1.py
@@ -0,0 +1,15 @@
+print("""
+Eniola Samuel
+September 26th 2022
+
+I am signing up for Replit's 100 days of python
+challenge!
+
+I will make sure to spend some time everyday codinng 
+along, for a minimum of 1hour a day.
+
+I will be using Replit, an amazing online IDE so I can do this from my phone whereve I happen to be. No excuses for not coding from the middle of a field!
+I am feeling 😎
+
+You can follow my progress at https://replit.com/@cliqueengagemen
+""")
